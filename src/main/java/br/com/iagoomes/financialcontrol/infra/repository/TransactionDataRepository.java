02189fb9c6b0e279c1ffc1a6ba1a349f79ec539d
Iago Gomes <iagoomes@outlook.com>
2025-10-13 21:28:42 -0300

    feat(users): implement user management with CRUD operations and user-specific data retrieval

--- src/main/java/br/com/iagoomes/financialcontrol/infra/repository/TransactionDataRepository.java
+++ src/main/java/br/com/iagoomes/financialcontrol/infra/repository/TransactionDataRepository.java
@@ -3,11 +3,9 @@ package br.com.iagoomes.financialcontrol.infra.repository;
 import br.com.iagoomes.financialcontrol.infra.repository.entity.TransactionData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TransactionDataRepository extends JpaRepository<TransactionData, String> {
